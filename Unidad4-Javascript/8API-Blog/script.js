0005fe31f31491a17fced8cee6d49e7422b2a5df
Sergio Castellote <scastellotec@iespabloserrano.com>
2024-01-19 13:09:45 +0100

    api

---
+++ Unidad4-Javascript/8API-Blog/script.js
@@ -0,0 +1,21 @@
+const urlApi = "https://jsonplaceholder.typicode.com";
+const mainSection = document.getElementById("posts");
+
+// Realizamos la llamada a la API
+fetch(urlApi+"/posts") // Esto hace la llamada a la URL
+  .then(function(response) {
+    return response.json(); // Cuando responde, formateo la respuestas como JSON
+  })
+  .then(function(json) { // En este bloque hago lo que quiero con el JSON
+    for(let i=0; i < json.length; i++){
+        let articlePost = document.createElement("article");
+        let articleTitulo = document.createElement("h2");
+        let articleBody = document.createElement("p");
+        articleTitulo.innerHTML = json[i].title;
+        articleBody.innerHTML = json[i].body;
+        articlePost.appendChild(articleTitulo);
+        articlePost.appendChild(articleBody);
+        mainSection.appendChild(articlePost);
+    }
+  });
+
